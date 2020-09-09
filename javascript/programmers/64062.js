f99e74f096f9a30dac645d8c0fefd297aee5789a
Terada <55175301+TERADA-DANTE@users.noreply.github.com>
2020-09-09 13:48:51 +0900

    solve 64062.js

--- javascript/programmers/64062.js
+++ javascript/programmers/64062.js
@@ -1,23 +1,37 @@
 function solution(stones, k) {
-  let [l, r, m] = [0, Math.max.apply(null, stones)]
-  while (l <= r) {
-    m = parseInt((l + r) / 2)
-    if (isCrossable(stones, m, k)) {
-      l = m + 1
-    } else {
-      r = m - 1
+  stones.push(Infinity)
+  let stack = [{ count: Infinity, idx: -1 }]
+  let answer = Infinity
+  for (let i = 0; i < stones.length; i++) {
+    console.log(stack, answer)
+    const right = { count: stones[i], idx: i }
+    while (stack[stack.length - 1].count < right.count) {
+      const mid = stack.pop()
+      const left = stack[stack.length - 1]
+      if (right.idx - left.idx > k) {
+        answer = Math.min(answer, mid.count)
+      }
     }
+    stack.push(right)
   }
-  for (let i = 0; i < 6974; i++)
-    if (!isCrossable(stones, r + i, k)) return r + i - 1
+  return answer
 }
 
-function isCrossable(array, n, k, cnt = 0) {
-  for (let i of array) {
-    n >= i ? cnt++ : (cnt = 0)
-    if (cnt > k) return false
-  }
-  return true
-}
+// function solution(stones, k) {
+//   let [l, r] = [0, 200000000]
+//   while (l <= r) {
+//     let mid = parseInt((l + r) / 2)
+//     isCrossable(stones, mid, k) ? (l = mid + 1) : (r = mid - 1)
+//   }
+//   return r + 1
+// }
+
+// function isCrossable(array, n, k, cnt = 0) {
+//   for (let i of array) {
+//     n >= i ? cnt++ : (cnt = 0)
+//     if (cnt >= k) return false
+//   }
+//   return true
+// }
 
 console.log(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
