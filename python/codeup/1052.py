9985caffac0fa13b197f894e39eedf2f3525f9e0
Terada <55175301+TERADA-DANTE@users.noreply.github.com>
2020-09-18 14:23:02 +0900

    solve codeup equivalency

--- python/codeup/1052.py
+++ python/codeup/1052.py
@@ -1,3 +1,3 @@
 a, b = list(map(int, input().split()))
 
-print(1 if a > b else 0)
+print(1 if a != b else 0)
--- python/codeup/index.js
+++ python/codeup/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-for (let i = 1004; i < 1050; i++) {
+for (let i = 1051; i < 1100; i++) {
   fs.writeFile(`./${i}.py`, "", function (err) {
     if (err) return console.log(err)
     console.log(i, "done")
