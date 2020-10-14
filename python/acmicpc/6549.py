e3a02334a88b0e812ede7489c0c95d91fcc933b1
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-10-14 18:02:48 +0900

    unsolve 6549.py devide and conquer

--- python/acmicpc/6549.py
+++ python/acmicpc/6549.py
@@ -1,27 +1,24 @@
-from collections import deque
-answer = []
 
-
-def solution(n, h):
-    dp = [None] * n
-    heights = deque(enumerate(h))
-    stack = deque([heights.popleft()])
-    while heights:
-        idx, val = heights.popleft()
-        if val < stack[-1][1]:
-            while stack:
-
-                length = len(stack)
-                index, value = stack.popleft()
-                dp[index] = value * length
-        stack.append((idx, val))
-    print(dp, stack)
-    return dp
+def solution(h):
+    mid = min(h) * len(h)
+    left = h[0:int(len(h)//2)] if h[0:int(len(h)//2)] else 0
+    right = h[int(len(h)//2):] if h[int(len(h)//2):] else 0
+    print(h, left, right)
+    if len(left) == 1:
+        left = left[0]
+    else:
+        left = solution(left)
+    if len(right) == 1:
+        right = right[0]
+    else:
+        right = solution(right)
+    print('max', max(mid, left, right), 'in', left, mid, right)
+    return max(mid, left, right)
 
 
 while True:
-    n, *h = list(map(int, input().split()))
-    if not n:
+    inputs = list(map(int, input().split()))
+    if not inputs[0]:
         break
-    answer.append(solution(n, h))
-print('\n'.join(list(map(str, answer))))
+    n, *h = inputs
+    print(solution(h))
