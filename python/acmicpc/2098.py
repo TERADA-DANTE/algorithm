27e8dfd3512e29abf0aac0abc60a07a00e010674
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-10-20 00:03:37 +0900

    unsolve 2098.py need to study dp dfs

--- python/acmicpc/2098.py
+++ python/acmicpc/2098.py
@@ -1,16 +1,27 @@
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 
+answer = float('inf')
+
 
 def solution(n, board):
-    return 1 << 0
-    # 0 0 0에 있고, 0을 방문한상태
-    # 1 2 3 4
-    # 1 2 4 3
-    # 1 3 2 4
-    # 1 3 4 2
-    # 1 4 2 3
-    # 1 4 3 2
+    dp = [[None] * (2**n+1) for _ in range(n+1)]
+
+    def execute(v=1, e=1 << 0):
+        global answer
+        if e == (1 << n) - 1:
+            if board[v-1][0]:
+                return board[v-1][0]
+            return float('inf')
+        if dp[v][e]:
+            return dp[v][e]
+        for i in range(n):
+            if (1 << i & e) == 0 and board[v-1][i]:
+                dp[v][e] = min(dp[v][e], execute(
+                    i+1, i << i | e) + board[v][i+1])
+
+    execute()
+    return answer
 
 
 print(solution(n, board))
