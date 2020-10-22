518fd33ba56b27afad758ff56cb75bd73246bcb5
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-10-22 16:39:31 +0900

    solve 3056.py with dp and bitmask simple is not always better

--- python/acmicpc/3056.py
+++ python/acmicpc/3056.py
@@ -1,24 +1,30 @@
-from itertools import permutations
+from itertools import combinations
+import sys
+input = sys.stdin.readline
 n = int(input())
 
-agents = [list(map(int, input().split())) for _ in range(n)]
-
-candidates = list(permutations(range(n), n))
+agents = [list(map(lambda v: int(v)/100, input().split())) for _ in range(n)]
 
 
-def mul(tuples):
-    result = 1
+def solution(n, agents):
+    dp = [0] * (2**n)
     for i in range(n):
-        result *= float(agents[i][tuples[i]]/100)
-    return result
+        dp[1 << i] = agents[i][0]
+    for cnt in range(1, n+1):
+        for number in range(int('1'*cnt, 2), int('1'*cnt+'0'*(n-cnt), 2)+1):
+            temp = number
+            c = 0
+            while temp:
+                c += temp % 2
+                temp = int(temp//2)
+            if c == cnt:
+                for shift in range(n):
+                    bit = 1 << shift
+                    if not number & bit:
+                        dp[number | bit] = max(
+                            dp[number | bit], (dp[number] if dp[number] else 1)*agents[shift][cnt])
+
+    return dp[-1]*100
 
 
-def solution(agents, candidates):
-    answer = 0
-    for candidate in candidates:
-        ret = mul(candidate)*100
-        answer = max(answer, ret)
-    return answer
-
-
-print('%0.6f' % float(solution(agents, candidates)))
+print((solution(n, agents)))
