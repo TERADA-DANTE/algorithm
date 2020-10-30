9a8142441c0e5b835cebdffe8593678484c8a8ea
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-10-30 17:55:21 +0900

    unsolve 2494.py with dp

--- python/acmicpc/2494.py
+++ python/acmicpc/2494.py
@@ -1,12 +1,45 @@
 from collections import deque
 n = int(input())
-initial = int(input())
-target = int(input())
+initial = input()
+target = input()
 
 
 def solution(n, initial, target):
-    dp = [0] * ((9*10**(n-1))+1)
-    queue = [[0, initial]]
+    dp = {}
+    queue = deque([[0, initial, []]])
+
+    while queue:
+        cnt, stringNumber, history = queue.popleft()
+
+        if dp.get(stringNumber):
+            continue
+        dp[stringNumber] = cnt
+        if stringNumber == target:
+            answer = {}
+            for i in range(len(history)):
+                t = f'{history[i][0]} {history[i][1]}'
+                if answer.get(t):
+                    answer[t] += history[i][1]
+                else:
+                    answer[t] = history[i][1]
+            ret = [f'{s[0]} {answer[s]}' for s in list(answer.keys())]
+            return str(cnt)+'\n'+'\n'.join(ret)
+        # 왼쪽으로
+        for i in range(n):
+            head = stringNumber[:i] if stringNumber else ['']
+            tail = [str(int(number)+1)[-1] for number in stringNumber[i:]]
+            newNumber = ''.join(head)+''.join(tail)
+
+            queue.append([cnt+1, newNumber, history+[[i+1, 1]]])
+
+        # 오른쪽으로
+        for i in range(n):
+            head = stringNumber[:i] if stringNumber else ['']
+            body = str(10+int(stringNumber[i])-1)[-1]
+            tail = stringNumber[i+1:]
+            newNumber = ''.join(head)+body+''.join(tail)
+
+            queue.append([cnt+1, newNumber, history+[[i+1, -1]]])
 
 
 print(solution(n, initial, target))
