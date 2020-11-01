57b9081efb00fcc32c833dbd238437d2a6710d17
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-11-02 00:37:23 +0900

    unsolve 2479.py Use toggle

--- python/acmicpc/2479.py
+++ python/acmicpc/2479.py
@@ -1,40 +1,34 @@
-from itertools import combinations
+from heapq import heappop, heappush
 from collections import deque
+import sys
+input = sys.stdin.readline
 n, k = list(map(int, input().split()))
-numbers = [input() for _ in range(n)]
+numbers = [input().rstrip() for _ in range(n)]
 s, e = list(map(int, input().split()))
 
-arr = list(combinations([i for i in range(n)], 2))
 
-board = [[0] * n for _ in range(n)]
-
-
-def hamming(path, number, k):
-    # 비트 마스킹
-    p = '0b1'+path
-    t = '0b1'+number
-    cnt = 0
-    for i in range(k):
-        if path[i] != number[i]:
-            cnt += 1
-    return cnt
-
-
-for a, b in arr:
-    board[a][b] = board[b][a] = hamming(numbers[a], numbers[b], k)
+def hamming(current, number):
+    a = int('1'+current, 2)
+    b = int('1' + number, 2)
+    return bin(a ^ b).count('1') == 1
 
 
 def solution(n, k, numbers):
-    start, end = numbers[s-1], numbers[e-1]
-    queue = deque([[start, [s-1]]])
-    while queue:
-        path, history = queue.popleft()
-        a = history[-1]
-        if path == end:
-            return list(map(lambda v: v+1, history))
-        for i in range(n):
-            if board[a][i] == 1 and i not in history:
-                queue.append([numbers[i], history+[i]])
+    board = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            if hamming(numbers[i], numbers[j]):
+                board[i].append(j+1)
+                board[j].append(i+1)
+    heap = [(0, [s], numbers[s-1])]
+    while heap:
+        cnt, history, number = heappop(heap)
+        h = history[-1]
+        if number == numbers[e-1]:
+            return history
+        for v in board[h-1]:
+            if v not in history:
+                heappush(heap, (cnt+1, history+[v], numbers[v-1]))
     return [-1]
 
 
