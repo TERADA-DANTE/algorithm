338f892747504fb2dfdf41bf1943eacdddb373ed
TERADA-DANTE <terada.syun.kim@gmail.com>
2020-11-09 16:12:21 +0900

    unsolve 2612.py

--- python/acmicpc/1168.py
+++ python/acmicpc/1168.py
@@ -1,16 +1,11 @@
 import time
-from collections import deque
-from heapq import heappush, heappop
+
 n, k = list(map(int, input().split()))
 
 
 def solution(n, k):
-    dq = deque([i for i in range(1, n+1)])
-    answer = []
-    for _ in range(n):
-        dq.rotate(-k+1)
-        answer.append(dq.popleft())
-    return '<' + ', '.join(map(str, answer)) + '>'
+    numbers = [i for i in range(1, n+1)]
+    return 1
 
 
 s = time.time()
--- python/acmicpc/2612.py
+++ python/acmicpc/2612.py
@@ -2,7 +2,34 @@ a = int(input())
 A = list(input())
 b = int(input())
 B = list(input())
+
+
 def solution(a, b, A, B):
+    dp = [[0]*(a+1) for _ in range(b+1)]
+    for i in range(b+1):
+        for j in range(a+1):
+            if not i:
+                if j:
+                    dp[i][j] = -2*j
+            elif not j:
+                if i:
+                    dp[i][j] = -2*i
+            else:
+                candidates = [dp[i-1][j], dp[i][j-1], dp[i-1][j-1]]
+                if B[i-1] == A[j-1]:
+                    print(i, j)
+                    dp[i][j] = max(dp[i-1][j-1]+3, 3)
+                else:
+                    dp[i][j] = max(candidates)-2
+    for i in range(b+1):
+        print(*dp[i])
+    answer = 0
+    for i in range(b+1):
+        for j in range(a+1):
+            if answer < dp[i][j]:
+                answer = dp[i][j]
+                row, col = i, j
+    return answer, row, col
 
 
 print(solution(a, b, A, B))
